6912cf85eb7ea3bb6ec3ed02dad831f1cfc37ef7
ricardogsilva <ricardo.garcia.silva@gmail.com>
2014-02-26 17:36:10 +0000

    added some example code for interacting with the CSW server

--- pyoseo/tasks.py
+++ pyoseo/tasks.py
@@ -1,5 +1,27 @@
 '''
 Celery tasks for pyoseo
+
+Example code for retrieving a CSW record once we know its catalog id:
+
+from lxml import etree
+import requests
+import pyxb.bundles.opengis.csw_2_0_2 as csw
+
+req = csw.GetRecordsByÎd(service='CSW', version='2.0.2')
+req.elementSetName = 'brief'
+req.Id.append('3b584f68-8e05-11e3-b102-0019995d2a58')
+response = requests.post(
+    'http://geoland2.meteo.pt/geonetwork/srv/eng/csw',
+    data=req.toxml(),
+    headers={'Content-Type': 'application/xml'}
+)
+nsmap = {
+    'csw': 'http://www.opengis.net/cat/csw/2.0.2'
+    'dc': 'http://purl.org/dc/elements/1.1/'
+    'ows': 'http://www.opengis.net/ows'
+}
+resp = etree.fromstring(response.text.encode(response.encoding))
+item_title = resp.xpath('csw:BriefRecord/dc:title/text()', namespaces=nsmap)
 '''
 
 import time
