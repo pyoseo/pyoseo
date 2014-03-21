224808605e34c603539abc547b0af1cfd274e0ec
Ricardo Garcia Silva <ricardo.garcia.silva@gmail.com>
2014-03-21 12:11:50 +0000

    created a view to serve order items

--- pyoseo/oseoserver/urls.py
+++ pyoseo/oseoserver/urls.py
@@ -4,4 +4,6 @@ from oseoserver import views
 urlpatterns = patterns(
     '',
     url(r'^server$', views.oseo_endpoint, name='oseo_endpoint'),
+    url(r'^orders/(?P<username>\w+)/(?P<order_id>\i+)/(?P<order_item_id>\i+)/$',
+        views.oseo_endpoint, name='show_item'),
 )
--- pyoseo/oseoserver/views.py
+++ pyoseo/oseoserver/views.py
@@ -1,7 +1,13 @@
+import os
+
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from django.views.decorators.csrf import csrf_exempt
+from django.core.servers.basehttp import FileWrapper
+from django.core.exceptions import ObjectDoesNotExist
+
 from oseoserver import server
+from oseoserver import models
 
 @csrf_exempt
 def oseo_endpoint(request):
@@ -11,6 +17,37 @@ def oseo_endpoint(request):
         response = HttpResponse(resp)
         for k,v in headers.iteritems():
             response[k] = v
+    else:
+        raise Http404
+    return response
+
+def show_item(request, username, order_id, item_file_name):
+    '''
+    Return the already available order item
+
+    The order item download count will be incremented and its status
+    set to DOWNLOADED.
+    '''
+
+    try:
+        order_item = models.OrderItem.get(file_name=item_file_name)
+    except ObjectDoesNotExist:
+        raise Http404
+    orders_root_dir = '/var/www' # find a nice way to set this value
+    item_path = os.path.join(orders_root_dir, username, order_id,
+                             item_file_name)
+    if os.path.isfile(item_path):
+        if item_file_name.endswith('tar.bz2'):
+            ct = 'application/x-tar'
+        elif item_file_name.endswith('.bz2'):
+            ct = 'application/x-bzip2'
+        response = HttpResponse(FileWrapper(open(item_path)),
+                                content_type=ct)
+        response['Content-Disposition'] = 'attachment; filename="%s"' % \
+            item_file_name
+        order_item.downloads += 1
+        order_item.status = models.CustomizableItem.DOWNLOADED
+        order_item.save()
     else:
         raise Http404
     return response
