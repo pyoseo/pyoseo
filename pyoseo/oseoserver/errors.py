a5bf3ff9fbd1bda9bf31f6ed4d5f49e9d82fd45b
ricardogsilva <ricardo.garcia.silva@gmail.com>
2014-04-01 20:13:58 +0100

    added a module with custom exception classes

---
+++ pyoseo/oseoserver/errors.py
@@ -0,0 +1,38 @@
+# Copyright 2014 Ricardo Garcia Silva
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+'''
+Custom exception classes for oseoserver
+'''
+
+class InvalidUserError(Exception):
+    pass
+
+class InvalidOrderDeliveryMethodError(Exception):
+    pass
+
+class DeliveryMethodNotAllowedError(Exception):
+    pass
+
+class OperationNotImplementedError(Exception):
+    pass
+
+class SubmitWithQuotationError(Exception):
+    pass
+
+class SubmitSubscriptionError(Exception):
+    pass
+
+class SubmitMassiveOrderError(Exception):
+    pass
