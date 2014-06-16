02fc0a195716f7f6c382c45ec7bede281d3a45fd
Ricardo Garcia Silva <ricardo.garcia.silva@gmail.com>
2014-06-16 14:39:12 +0100

    adding a mock class to conf.py in order to trick readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -15,6 +15,31 @@
 import sys
 import os
 
+class Mock(object):
+    __all__ = []
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return Mock()
+
+    @classmethod
+    def __getattr__(cls, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            mockType = type(name, (), {})
+            mockType.__module__ = __name__
+            return mockType
+        else:
+            return Mock()
+
+MOCK_MODULES = ['giosystemcore',]
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = Mock()
+
+
 sys.path.append(os.path.dirname(__file__))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 
